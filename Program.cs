68540099660c32cc750e7ed6afe6e65ecee0b09a
AlicaWalker2004 <daniella@email.com>
2024-04-25 13:37:19 +0400

    Kalkulyator

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿double Result = 0;
+Console.Write("Введите число A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B: ");
+double B = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите действие: ");
+string Deistvie = Console.ReadLine();
+if (Deistvie == "+")
+{
+    Result = A + B;
+}
+else if (Deistvie == "-")
+{
+    Result = A - B;
+}
+else if (Deistvie == "*")
+{
+    Result = A * B;
+}
+else if (Deistvie == "/")
+{
+    Result = A / B;
+}
+Console.WriteLine(Result);
